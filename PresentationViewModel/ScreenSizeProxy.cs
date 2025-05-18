666546d0adb9c1f7e9fd7a21d3b3586e88d292c7
Ntn333 <natan.renke@gmail.com>
2025-05-18 16:22:21 +0200

    task 2.1.1 screen size proxy

---
+++ PresentationViewModel/ScreenSizeProxy.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+namespace TP.ConcurrentProgramming.Presentation.ViewModel
+{
+    public class ScreenSizeProxy : INotifyPropertyChanged
+    {
+        private double _width;
+        private double _height;
+
+        public double Width
+        {
+            get => _width;
+            set
+            {
+                if (_width != value)
+                {
+                    _width = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Width)));
+                }
+            }
+        }
+
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if (_height != value)
+                {
+                    _height = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Height)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
